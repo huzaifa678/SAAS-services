efd2b8865391889ec3bd3ecc8cfd73043714a891
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-02-03 17:38:27 +0500

    added graceful handling and changed server such that

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,11 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
 import promClient from 'prom-client';
-import router from '../auth-service/src/controller/auth.controller.js'
+import router from '../auth-service/src/controller/auth.controller.js';
+import http from 'http';
 
 const app = express();
-app.use(express.json());
+const metricsApp = express();
 
+
+const METRICS_PORT = 4001;
+
+app.use(express.json());
 app.use('/api/auth', router);
 
 app.get('/metrics', async (req, res) => {
@@ -13,10 +18,37 @@ app.get('/metrics', async (req, res) => {
   res.end(await promClient.register.metrics());
 });
 
-app.listen(8080, () => {
-  console.log("listening to port 8080")
-})
+const server = http.createServer(app);
+const metricsServer = http.createServer(metricsApp);
 
-app.listen(4001, () =>
-  console.log('Prometheus metrics available at http://localhost:4001/metrics')
-);
+metricsServer.listen(METRICS_PORT, () => {
+  console.log(`Prometheus metrics available at http://localhost:${METRICS_PORT}/metrics`);
+});
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const shutdownSignals = ['SIGINT', 'SIGTERM', 'SIGHUP', 'SIGQUIT'];
+
+shutdownSignals.forEach(sig => {
+  process.on(sig, () => {
+    console.log(`Received ${sig}, shutting down gracefully...`);
+
+    server.close(err => {
+      if (err) {
+        console.error('Error during server shutdown', err);
+        process.exit(1);
+      }
+
+      console.log('Server closed gracefully');
+      process.exit(0);
+    })
+
+    setTimeout(() => {
+      console.warn('Forcing shutdown...');
+      process.exit(1);
+    }, 5000);
+  })
+})
