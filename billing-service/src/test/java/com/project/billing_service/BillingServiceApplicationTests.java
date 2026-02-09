bd4a7701df48d8434d54e2a7424d861eeebc0cc0
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-02-09 13:36:18 +0500

    added avro schame in billing service and configured test for billing service

--- billing-service/src/test/java/com/project/billing_service/BillingServiceApplicationTests.java
+++ billing-service/src/test/java/com/project/billing_service/BillingServiceApplicationTests.java
@@ -1,15 +1,50 @@
 package com.project.billing_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.data.jdbc.test.autoconfigure.AutoConfigureDataJdbc;
+import org.springframework.boot.data.jdbc.test.autoconfigure.DataJdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class BillingServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ServiceConnection
+    @Container
+    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+
+
+    @ServiceConnection
+    @Container
+    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"));
+
+
+    @Container
+    static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7"))
+            .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
