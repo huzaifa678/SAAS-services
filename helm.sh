2b7f483176134d9bebbef071cfbbb6597a3b441d
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-02-19 05:35:48 +0500

    added content of the chart values for values.yaml and added service.yaml for external redis

--- helm.sh
+++ helm.sh
@@ -3,9 +3,8 @@
 # helm repo add apache-airflow https://airflow.apache.org
 # helm repo update
 
-helm upgrade --install airflow apache-airflow/airflow \
+helm upgrade --install airflow airflow-helm/airflow \
   --namespace airflow --create-namespace \
-  -f values.yaml \
   --set executor=CeleryExecutor \
   --set dags.persistence.enabled=true \
   --set dags.persistence.size=2Gi \
@@ -13,14 +12,11 @@ helm upgrade --install airflow apache-airflow/airflow \
   --set logs.persistence.size=1Gi \
   --set scheduler.logCleanup.enabled=false \
   --set workers.logCleanup.enabled=false \
-  --set postgresql.enabled=true \
-  --set postgresql.image.registry=docker.io \
-  --set postgresql.image.repository=postgres \
-  --set postgresql.image.tag=15-alpine \
   --set workers.replicas=1 \
   --set ingress.enabled=true \
   --set ingress.className=nginx \
   --set ingress.web.hosts[0].host=airflow.local \
   --set ingress.web.hosts[0].paths[0].path="/" \
-  --set ingress.web.hosts[0].paths[0].pathType=Prefix
+  --set ingress.web.hosts[0].paths[0].pathType=Prefix \
+  -f values.yaml
 
