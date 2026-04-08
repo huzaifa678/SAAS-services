71323f9732c0755db87bd490ec04c9f901c76b8a
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-04-08 19:20:28 +0500

    updated logstash config replacing tcp with http for log exports and adding filter

--- billing-service/src/test/java/com/project/billing_service/BillingServiceApplicationTests.java
+++ billing-service/src/test/java/com/project/billing_service/BillingServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.project.billing_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
