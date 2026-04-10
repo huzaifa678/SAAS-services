f69a68f804a9166796e2edffb3e00d04dbca06b4
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-04-11 02:16:39 +0500

    added error.go

---
+++ api-gateway/errors/error.go
@@ -0,0 +1,21 @@
+package errors
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+func encodeError(_ context.Context, err error, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	
+	if err.Error() == "rate limit exceeded" || err.Error() == "service busy (storage pressure)" {
+		w.WriteHeader(http.StatusServiceUnavailable) // 503
+	} else {
+		w.WriteHeader(http.StatusInternalServerError) // 500
+	}
+    
+	json.NewEncoder(w).Encode(map[string]string{
+		"error": err.Error(),
+	})
+}
